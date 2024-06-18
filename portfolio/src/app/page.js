1be2f007dc37940cdf18c047c592e5f8ffdb67c5
Séraphin <seraphinbnt@gmail.com>
2024-06-18 11:34:27 +0200

    align text in the title

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-3xl">Seraphino Portfolio</h1>
-      <h3 className="mt-6">Coming soon</h3>
+      <h1 className="text-2xl md:text-3xl text-center">Seraphino Portfolio</h1>
+      <h3 className="mt-6 text-center">Coming soon</h3>
     </main>
   );
 }
