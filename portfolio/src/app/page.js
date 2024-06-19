157430594ca32ee68f6b1d9fbf9094440fdd48e8
Séraphin <seraphinbnt@gmail.com>
2024-06-19 12:42:21 +0200

    change seraphino to seraphin

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-2xl md:text-3xl text-center">Seraphino Portfolio</h1>
+      <h1 className="text-2xl md:text-3xl text-center">Seraphin Portfolio</h1>
       <h3 className="mt-6 text-center">Coming soon</h3>
     </main>
   );
