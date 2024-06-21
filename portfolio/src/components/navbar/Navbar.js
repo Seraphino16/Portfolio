d4493e0e4725855b97ce6c7b595754af20b52632
Séraphin <seraphinbnt@gmail.com>
2024-06-21 17:55:46 +0200

    update text size in navbar sm

--- portfolio/src/components/navbar/Navbar.js
+++ portfolio/src/components/navbar/Navbar.js
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return(
         <div className="flex items-center justify-around p-8 absolute top-0 w-full">
-            <h4 className="text-2xl md:text-5xl w-6/ text-center">Portfolio</h4>
+            <h4 className="text-3xl md:text-5xl w-6/ text-center">Portfolio</h4>
             <div className="hidden md:flex justify-between w-4/12 text-xl">
                 <a href="#">À propos</a>
                 <a href="#projects">Projets</a>
