001e940f5fbac484e2eb1d4ca8e1e23f36529b94
Séraphin Benoit <seraphin.benoit@adeo.com>
2025-03-13 16:20:57 +0100

    feat(navbar): hide the navbar on scroll to bottom

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -8,6 +8,10 @@ import {Home, Mail, Terminal, User} from "feather-icons-react";
 const Navbar: React.FC = () => {
 
     const [activeLink, setActiveLink] = useState('hero');
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [isVisible, setIsVisible] = useState(true);
+    const [clicked, setClicked] = useState(false);
+    const [isScrolling, setIsScrolling] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -20,32 +24,58 @@ const Navbar: React.FC = () => {
                 }
             });
             setActiveLink(currentSection);
+
+            if (clicked || isScrolling) {
+                setIsVisible(true);
+                return;
+            }
+
+            console.log(clicked);
+            const currentScrollY = window.scrollY;
+            if (currentScrollY > lastScrollY && !clicked) {
+                setIsVisible(false);
+            } else {
+                setIsVisible(true);
+            }
+            setLastScrollY(currentScrollY);
+
         };
 
         window.addEventListener('scroll', handleScroll);
 
         return () => window.removeEventListener('scroll', handleScroll);
-    })
+    });
+
+    const handleClicked = () => {
+        setClicked(true);
+        setIsVisible(true);
+        setIsScrolling(true);
+
+        setTimeout(() => {
+            setClicked(false);
+            setIsScrolling(false);
+        }, 1000);
+    }
 
     return (
-        <div className={navbar.container}>
-            <nav className={navbar.navbar}>
-                <Link href={'#hero'} className={activeLink === 'hero' ? navbar.navbarActive : ''}>
+        <div className={`${navbar['container']} ${isVisible ? navbar['visible'] : navbar['hidden']}`}>
+            <nav className={`${navbar['navbar']} ${isVisible ? navbar['visible'] : navbar['hidden']}`}>
+                <Link onClick={handleClicked} href={'#hero'} className={activeLink === 'hero' ? navbar.navbarActive : ''}>
                     <div className={navbar.navbarItem}>
                         <Home size={30}/>
                     </div>
                 </Link>
-                <Link href={'#about'} className={activeLink === 'about' ? navbar.navbarActive : ''}>
+                <Link onClick={handleClicked} href={'#about'} className={activeLink === 'about' ? navbar.navbarActive : ''}>
                     <div className={navbar.navbarItem}>
                         <User size={30} />
                     </div>
                 </Link>
-                <Link href={'#projects'} className={activeLink === 'projects' ? navbar.navbarActive : ''}>
+                <Link onClick={handleClicked} href={'#projects'} className={activeLink === 'projects' ? navbar.navbarActive : ''}>
                     <div className={navbar.navbarItem}>
                         <Terminal size={30} />
                     </div>
                 </Link>
-                <Link href={'#form'} className={activeLink === 'form' ? navbar.navbarActive : ''}>
+                <Link onClick={handleClicked} href={'#form'} className={activeLink === 'form' ? navbar.navbarActive : ''}>
                     <div className={navbar.navbarItem}>
                         <Mail size={30} />
                     </div>
